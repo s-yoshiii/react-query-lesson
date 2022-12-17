5a498f974e1035d69013042b54776620637fea41
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-17 22:01:00 +0900

    Classical sever data state management Provider

---
+++ src/context/StateProvider.tsx
@@ -0,0 +1,20 @@
+import { useContext, useState, createContext, FC } from 'react'
+import { Task } from '../types/types'
+interface StateContextType {
+  tasks: Task[] | null
+  dark: boolean
+  setTasks: React.Dispatch<React.SetStateAction<Task[] | null>>
+  setDark: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SteteContext = createContext({} as StateContextType)
+export const StateProvider: FC = ({ children }) => {
+  const [tasks, setTasks] = useState<Task[] | null>(null)
+  const [dark, setDark] = useState(false)
+  return (
+    <SteteContext.Provider value={{ tasks, setTasks, dark, setDark }}>
+      {children}
+    </SteteContext.Provider>
+  )
+}
+export const useStateContext = (): StateContextType => useContext(SteteContext)
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export interface Task {
+  id: number
+  title: string
+  tag: number
+  tag_name: string
+  create_at: string
+  update_at: string
+}
