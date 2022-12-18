9c36d61549d22d647bac9c3f86bfa03ccd03fb75
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-19 00:16:09 +0900

    Classical server data state management hooks

---
+++ src/components/ClassicalFetchA.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+import { useClassicalFetch } from '../hooks/useClassicalFetch'
+import { useNavigate } from 'react-router-dom'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+export const ClassicalFetchA: FC = () => {
+  return <div>ClassicalFetchA</div>
+}
