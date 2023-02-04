dc55099882afc009dd5382acfb4e195ac200eea6
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-02-05 00:57:56 +0900

    RTKitAComponent作成

--- src/components/RTKitA.tsx
+++ src/components/RTKitA.tsx
@@ -5,8 +5,19 @@ import { increment, selectCount } from '../slices/counterSlice'
 const RTKitA: FC = () => {
   const count = useAppSelector(selectCount)
   const dispatch = useAppDispatch()
-  console.log('rendered RTKit')
-  return <div></div>
+  console.log('rendered RTKitA')
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <p className="font-bold my-3">RTKitA</p>
+      {count}
+      <button
+        className="py-2 px-3 mt-3 text-sm text-white bg-indigo-600 hover:bg-indigo-700 rounded"
+        onClick={() => dispatch(increment())}
+      >
+        increment
+      </button>
+    </div>
+  )
 }
 
 export default RTKitA
