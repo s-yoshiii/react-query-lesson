13fe1126592adb0c723b5fad82b3993c29c46fe0
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-02-07 00:11:37 +0900

    Beauty of redux-toolkit

--- src/components/RTKitC.tsx
+++ src/components/RTKitC.tsx
@@ -7,7 +7,7 @@ const RTKitC: FC = () => {
   console.log('rendered RTKitC')
   return (
     <div className="flex justify-center items-center flex-col">
-      <p className="font-bold my-3">RTKitB</p>
+      <p className="font-bold my-3">RTKitC</p>
       <button
         className="py-2 px-3 mt-3 text-sm text-white bg-indigo-600 hover:bg-indigo-700 rounded"
         onClick={() => {
